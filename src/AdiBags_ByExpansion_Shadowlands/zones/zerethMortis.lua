989807532a3c4db89fdc5de19015cb311f212dff
me <here>
2022-11-30 23:07:18 +0000

    add some Zereth Mortis items

---
+++ src/AdiBags_ByExpansion_Shadowlands/zones/zerethMortis.lua
@@ -0,0 +1,20 @@
+local AddonName, AddonTable = ...
+
+-- zerethMortis (Shadowlands)
+AddonTable.zerethMortis = {
+    -- crafting stuff
+    187707,
+    187728,
+    187789,
+    187790,
+    187791,
+    187810,
+    187885,
+    187889,
+    187892,
+    187893,
+    189863,
+    190189,
+    190198,
+    190239
+}
