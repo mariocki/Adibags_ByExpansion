49501c0b80331a7c275b78652d3b98f80ab9868a
me <here>
2022-12-04 13:53:24 +0000

    more DF items

--- src/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
+++ src/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
@@ -7,6 +7,7 @@ AddonTable.jewelcrafting = {
     192845, -- Vibrant Emerald
     192846, -- Sundered Onyx
     192849, -- Eternity Amber
+    192850, -- Eternity Amber
     192851, -- Eternity Amber
     192852, -- Alexstraszite
     192856, -- Malygite
@@ -16,5 +17,9 @@ AddonTable.jewelcrafting = {
     192869, -- Illimited Diamond
     192872, -- Fractured Glass
     192880, -- Crumbled Stone
+    193368, -- Silken Gemdust
+
+    -- Gems
+    192907, -- Energized Vibrant Emerald
 
 }
--- src/AdiBags_ByExpansion_Dragonflight/professions/gathering/mining.lua
+++ src/AdiBags_ByExpansion_Dragonflight/professions/gathering/mining.lua
@@ -6,6 +6,8 @@ AddonTable.mining = {
     189143, -- Draconium Ore
 
     190312, -- Khaz'gorite Ore
-    
+    190313, -- Khaz'gorite Ore
+
+    190395, -- Serevite Ore
     190396, -- Serevite Ore
 }
