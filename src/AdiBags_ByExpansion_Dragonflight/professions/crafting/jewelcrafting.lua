cd338eaa28066b6cc30a43247b8027f84101dbf3
me <here>
2022-12-15 23:32:08 +0000

    DF jewels

--- src/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
+++ src/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
@@ -1,26 +1,32 @@
 local AddonName, AddonTable = ...
 AddonTable.jewelcrafting = {
     -- Uncut
-    192837, -- Queen's Ruby
-    192838, -- Queen's Ruby
-    192840, -- Mystic Sapphire
-    192841, -- Mystic Sapphire
-    192843, -- Vibrant Emerald
-    192844, -- Vibrant Emerald
-    192845, -- Vibrant Emerald
-    192846, -- Sundered Onyx
-    192847, -- Sundered Onyx
-    192849, -- Eternity Amber
-    192850, -- Eternity Amber
-    192851, -- Eternity Amber
+    192837, -- Queen's Ruby I
+    192838, -- Queen's Ruby II
+    192839, -- Queen's Ruby III
+    192840, -- Mystic Sapphire I
+    192841, -- Mystic Sapphire II
+    192842, -- Mystic Sapphire III
+    192843, -- Vibrant Emerald I
+    192844, -- Vibrant Emerald II
+    192845, -- Vibrant Emerald III
+    192846, -- Sundered Onyx I
+    192847, -- Sundered Onyx II
+    192848, -- Sundered Onyx III
+    192849, -- Eternity Amber I
+    192850, -- Eternity Amber II
+    192851, -- Eternity Amber III
     192852, -- Alexstraszite
     192853, -- Alexstraszite
-    192856, -- Malygite
-    192857, -- Malygite
-    192859, -- Ysemerald
-    192860, -- Ysemerald
-    192862, -- Neltharite
-    192863, -- Neltharite
+    192856, -- Malygite I
+    192857, -- Malygite II
+    192858, -- Malygite III
+    192859, -- Ysemerald I
+    192860, -- Ysemerald II
+    192861, -- Ysemerald III
+    192862, -- Neltharite I
+    192863, -- Neltharite II
+    192864, -- Neltharite III
     192866, -- Nozdorite
     192867, -- Nozdorite
     192869, -- Illimited Diamond
